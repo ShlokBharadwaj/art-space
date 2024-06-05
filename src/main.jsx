74ad920cd37531e21dbbc76ebc60f855576cee48
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-06-05 13:27:55 +0530

    Update: Consistent bg color

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function CenteredApp() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <div className="mx-auto max-w-6xl px-5 box-border">
+        <App />
+      </div>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CenteredApp />
   </React.StrictMode>,
 )
