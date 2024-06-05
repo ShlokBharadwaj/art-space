40d5f87db3c2e3cb3519a0d9a74efa55a3caee31
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-06-05 12:41:33 +0530

    Update: Right sidebar done

--- src/components/Home/ArtistsSidebar.jsx
+++ src/components/Home/ArtistsSidebar.jsx
@@ -9,30 +9,58 @@ const ArtistsSidebar = () => {
                 <ul>
                     <li className="relative mb-2">
                         <img src="./svgs/artistssidebar/pexels-ekaterina-12203460.png" alt="John Smith" className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-[0.15] rounded-md"></div>
                         <div className="absolute bottom-0 left-0 p-2 flex items-center">
-                            <img src="./svgs/artistssidebar/pexels-anastasia-shuraeva-4406721.png" alt="Thomas Edward" className="w-10 h-10 rounded-full mr-3" />
-                            Thomas Edward
+                            <div className="relative">
+                                <img src="./svgs/artistssidebar/pexels-anastasia-shuraeva-4406721.png" alt="Thomas Edward" className="w-10 h-10 rounded-md mr-3" />
+                                <div className="absolute -top-1 right-3 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
+                            </div>
+                            <div className="text-white">
+                                <p className="font-medium text-sm">Thomas Edward</p>
+                                <p className="text-xs">@thewildwithyou</p>
+                            </div>
                         </div>
                     </li>
                     <li className="relative mb-2">
                         <img src="./svgs/artistssidebar/pexels-genaro-servín-763210.png" alt="Jane Doe" className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-[0.15] rounded-md"></div>
                         <div className="absolute bottom-0 left-0 p-2 flex items-center">
-                            <img src="./svgs/artistssidebar/pexels-andrea-piacquadio-3771118.png" alt="Chris Doe" className="w-10 h-10 rounded-full mr-3" />
-                            Chris Doe
+                            <div className="relative">
+                                <img src="./svgs/artistssidebar/pexels-andrea-piacquadio-3771118.png" alt="Chris Doe" className="w-10 h-10 rounded-md mr-3" />
+                                <div className="absolute -top-1 right-3 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
+                            </div>
+                            <div className="text-white">
+                                <p className="font-medium text-sm">Chris Doe</p>
+                                <p className="text-xs">@thewildwithyou</p>
+                            </div>
                         </div>
                     </li>
                     <li className="relative mb-2">
                         <img src="./svgs/artistssidebar/pexels-pixabay-164455.png" alt="Alice Brown" className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-[0.15] rounded-md"></div>
                         <div className="absolute bottom-0 left-0 p-2 flex items-center">
-                            <img src="./svgs/artistssidebar/pexels-antoni-shkraba-4442102.png" alt="Emilie Jones" className="w-10 h-10 rounded-full mr-3" />
-                            Emilie Jones
+                            <div className="relative">
+                                <img src="./svgs/artistssidebar/pexels-antoni-shkraba-4442102.png" alt="Emilie Jones" className="w-10 h-10 rounded-md mr-3" />
+                                <div className="absolute -top-1 right-3 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
+                            </div>
+                            <div className="text-white">
+                                <p className="font-medium text-sm">Emilie Jones</p>
+                                <p className="text-xs">@thewildwithyou</p>
+                            </div>
                         </div>
                     </li>
                     <li className="relative mb-2">
                         <img src="./svgs/artistssidebar/pexels-steve-johnson-1269968.png" alt="Bob White" className="w-full" />
+                        <div className="absolute inset-0 bg-black opacity-[0.15] rounded-md"></div>
                         <div className="absolute bottom-0 left-0 p-2 flex items-center">
-                            <img src="./svgs/artistssidebar/pexels-antoni-shkraba-4442005.png" alt="Jessica Williams" className="w-10 h-10 rounded-full mr-3" />
-                            Jessica Williams
+                            <div className="relative">
+                                <img src="./svgs/artistssidebar/pexels-antoni-shkraba-4442005.png" alt="Jessica Williams" className="w-10 h-10 rounded-md mr-3" />
+                                <div className="absolute -top-1 right-3 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
+                            </div>
+                            <div className="text-white">
+                                <p className="font-medium text-sm">Jessica Williams</p>
+                                <p className="text-xs">@thewildwithyou</p>
+                            </div>
                         </div>
                     </li>
                 </ul>
